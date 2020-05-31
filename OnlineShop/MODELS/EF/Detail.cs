a0d2c0a4c030b569e423a00dcf02fafa8899d2f8
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2020-05-31 22:05:35 +0700

    reformat: delete libraries, document: change user case

--- OnlineShop/MODELS/EF/Detail.cs
+++ OnlineShop/MODELS/EF/Detail.cs
@@ -4,7 +4,6 @@ namespace MODELS.EF
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     [Table("Detail")]
     public partial class Detail
