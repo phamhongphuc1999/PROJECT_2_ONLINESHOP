6f9026bdd89039c82c4f5dded3de8dabf363e37a
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2020-06-18 14:22:54 +0700

    add: JS and CSS

--- PROJECT2/ManagementStore/App_Start/BundleConfig.cs
+++ PROJECT2/ManagementStore/App_Start/BundleConfig.cs
@@ -25,6 +25,39 @@ namespace ManagementStore
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/CSS").Include(
+                "~/plugins/fontawesome-free/css/all.min.css",
+                "~/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css",
+                "~/plugins/icheck-bootstrap/icheck-bootstrap.min.css",
+                "~/plugins/jqvmap/jqvmap.min.css",
+                "~/dist/css/adminlte.min.css",
+                "~/plugins/overlayScrollbars/css/OverlayScrollbars.min.css",
+                "~/plugins/daterangepicker/daterangepicker.css",
+                "~/plugins/summernote/summernote-bs4.min.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/JQUERY_NEW").Include(
+                "~/plugins/jquery/jquery.min.js",
+                "~/plugins/jquery-ui/jquery-ui.min.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/JS").Include(
+                "~/plugins/bootstrap/js/bootstrap.bundle.min.js",
+                "~/plugins/chart.js/Chart.min.js",
+                "~/plugins/sparklines/sparkline.js",
+                "~/plugins/jqvmap/jquery.vmap.min.js",
+                "~/plugins/jqvmap/maps/jquery.vmap.usa.js",
+                "~/plugins/jquery-knob/jquery.knob.min.js",
+                "~/plugins/moment/moment.min.js",
+                "~/plugins/daterangepicker/daterangepicker.js",
+                "~/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js",
+                "~/plugins/summernote/summernote-bs4.min.js",
+                "~/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js",
+                "~/dist/js/adminlte.js",
+                "~/dist/js/pages/dashboard.js",
+                "~/dist/js/demo.js"
+                ));
         }
     }
 }
